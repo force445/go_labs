76d3116db439bbac6d62a6a7eb4c25590fbf097f
Force <naphatnaphat85@gmail.com>
2023-12-13 13:37:36 +0700

    add credit card validator

--- go-posgres/validator/validator.go
+++ go-posgres/validator/validator.go
@@ -6,7 +6,7 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 
-	"go-posgres/handler"
+	"go-posgres/model"
 )
 
 func ValidateStruct(data interface{}) (bool, []string) {
@@ -29,6 +29,42 @@ func ValidateStruct(data interface{}) (bool, []string) {
 	return true, nil
 }
 
-func ValidateCreditCard() {
-	creditcard := handler.GetCreditCard()
+func ValidateCreditCard(creditCardData model.CreditCard) (bool, []string) {
+	validate := validator.New()
+	err := validate.Struct(creditCardData)
+
+	if err != nil {
+		var errors []string
+		for _, err := range err.(validator.ValidationErrors) {
+			errors = append(errors, fmt.Sprintf("%s is %s", err.Field(), err.Tag()))
+		}
+		return false, errors
+	}
+
+	if !luhnAlgorithm(creditCardData.CardNo) {
+		return false, []string{"Invalid credit card number"}
+	}
+
+	return true, nil
+}
+
+func luhnAlgorithm(cardNumber string) bool {
+	var sum int
+	var alternate bool = false
+
+	for i := len(cardNumber) - 1; i >= 0; i-- {
+		digit := int(cardNumber[i] - '0')
+
+		if alternate {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+
+		sum += digit
+		alternate = !alternate
+	}
+
+	return sum%10 == 0
 }
